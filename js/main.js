dcd2b48c4b8dc9a01963ff9ec4a6e48eda93c785
yj2222 <masjima2712@gmai.com>
2024-12-08 11:39:02 +0900

    カード回転アニメーション実装

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(function () {
+    // スクロールアニメーション
+    const animatedCls = 'is-animated';
+    $(window).on('scroll', function () {
+        $(`.js-animationPoint:not(.${animatedCls})`).each(function () {
+let elementTop = $(this).offset().top;
+if (elementTop === 0) {
+    elementTop = 999999;
+}
+
+            const windowHeight = $(window).height();
+            const scrollTop = $(window).scrollTop();
+            const triggerPoint = scrollTop + (windowHeight / 2);
+            if (elementTop <= triggerPoint) {
+                $(this).addClass(animatedCls);
+            }
+        });
+    });
+
+    // スムーススクロール
+    function smoothScroll(target, delay) {
+        let headerHeight = $("header").height();
+        $("html, body").animate({ scrollTop: target.offset().top - headerHeight }, delay);
+    }
+    $('a[href^="#"]').on("click", function (event) {
+        let target = $($(this).attr("href"));
+
+        if (target.length) {
+            event.preventDefault();
+            smoothScroll(target, 1000);
+        }
+    });
+
+    //タブ
+    let activeCls = "is-active";
+    $(".tabLists_tab").click(function () {
+        let tabId = $(this).data("tab");
+        if (tabId === "tab1" || tabId === "tab2") {
+            $('[data-tab="tab1"]').removeClass(activeCls);
+            $('[data-tab="tab2"]').removeClass(activeCls);
+        }
+        $('[data-tab="' + tabId + '"]').addClass(activeCls);
+
+        let scrollTarget = $('#' + $(this).parents('.js-tabParentBlock').attr('id'));
+        smoothScroll(scrollTarget, 0);
+    });
+
+    // アコーディオン
+    $(".js-accordion").click(function () {
+        $(this).next().slideToggle();
+        $(this).toggleClass("is-open");
+    });
+
+});
